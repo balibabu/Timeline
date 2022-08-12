a74e7d812c0f88d2318926d74d951e75dfb07147
Bali <balibabuchauhan1@gmail.com>
2022-08-12 22:05:04 +0530

    auto uploaded by bat file

---
+++ 2022/08_August/search2.py
@@ -0,0 +1,60 @@
+import os
+import re
+def getListOfFiles(dirName):
+    fTypes=['java','py','c','txt','inpynb','cpp']
+    listOfFile = os.listdir(dirName)
+    allFiles = list()
+    for item in listOfFile:
+        if re.search('^\.',item):
+            continue
+        fullPath = os.path.join(dirName, item)
+        if os.path.isdir(fullPath):
+            allFiles = allFiles + getListOfFiles(fullPath)
+        else:
+            if not os.path.splitext(item)[1][1:] in fTypes:
+                continue
+            allFiles.append(fullPath)
+    return allFiles
+
+def readfiles(fname):
+    f=open(fname)
+    temp= f.read()
+    return temp
+    
+def isPresent(regex,string):
+	x=re.search(regex,string,re.IGNORECASE)
+	return x!=None
+
+def main():
+    path1=r'D:\Works_Space\SOA_University'
+    path2=r'D:\Works_Space\Timeline'
+    x=getListOfFiles(path1)+getListOfFiles(path2)
+
+    regex=input('enter keyword(regex) ')
+    count=0
+    outcome=list()
+    for i in x:
+        try:
+            file=(readfiles(i))
+            if isPresent(regex,file):
+                outcome.append(i)
+                count+=1
+                print(count,'=>',i)
+        except:
+            pass
+    print('found match:',count)
+    print('Total file searched:',len(x))
+    if count==0:
+        print('No matching found')
+        choice = input('press enter to exit')
+    elif count==1:
+        print(readfiles(outcome[0]))
+        choice = input('press enter to exit')
+    else:
+        while(1):
+            choice = int(input('enter an option number or 0 to quit:'))
+            if choice == 0: break
+            print(readfiles(outcome[choice-1]))
+
+if __name__ == '__main__':
+    main()
