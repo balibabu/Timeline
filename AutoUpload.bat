fa05fcbcbce9a291d6aa3b4c915c54aebeefb223
Bali <balibabuchauhan1@gmail.com>
2022-08-12 18:37:20 +0530

    auto uploaded by bat file

---
+++ AutoUpload.bat
@@ -0,0 +1,14 @@
+@ECHO OFF
+ECHO Auto pulling, adding, commiting and pushing on Github.
+ECHO ===================================================================================
+D:
+cd Works_Space\Timeline
+git pull
+git status
+git add .
+git status
+git commit -m"auto uploaded by bat file"
+git push
+ECHO ===================================================================================
+ECHO Everything Done. Bye, Have a Good Day (•‿•)
+PAUSE
