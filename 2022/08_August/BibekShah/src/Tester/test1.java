205501059bd08fdbb56c26ff8bad45e36b12b2c7
Bali <balibabuchauhan1@gmail.com>
2022-08-23 22:05:06 +0530

    auto uploaded by bat file

--- 2022/08_August/BibekShah/src/Tester/test1.java
+++ 2022/08_August/BibekShah/src/Tester/test1.java
@@ -5,8 +5,12 @@ public class test1 {
 	public static void main(String[] args) {
 		int a=5;
 		int b=3;
-		a=b=1;
+//		a=b=1;
 		System.out.println(a+" "+b);
+		int z=a<b?a:b;
+		System.out.println(z);
+		
+		
 	}
 
 }
