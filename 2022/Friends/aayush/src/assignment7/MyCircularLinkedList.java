6b7e2ac79a5bba2e97298316aca114fc4cdd8550
Bali Babu Chauhan <balibabuchauhan1@gmail.com>
2022-12-10 22:00:09 +0530

    updating timeline

---
+++ 2022/Friends/aayush/src/assignment7/MyCircularLinkedList.java
@@ -0,0 +1,140 @@
+package assignment7;
+
+import java.util.Iterator;
+
+public class MyCircularLinkedList<E>  implements MyList<E>{
+
+	public void add(String string) {
+		
+	}
+
+	public void add(int i, String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void addLast(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void remove(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean contains(String string) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void get(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void getFirst() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void getLast() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void removeFirst() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void removeLast() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int indexOf(String string) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int lastIndexOf(String string) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void addFirst(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void set(int i, String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Iterator<E> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void add(int index, E e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int indexOf(Object e) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int lastIndexOf(E e) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public E set(int index, E e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void add(int index, E e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int lastIndexOf(E e) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public E set(int index, E e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
