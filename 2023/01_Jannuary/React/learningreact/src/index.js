51f6d05dec6ed522f756f3d1dd261c9f40d4e16e
Bali Babu Chauhan <balibabuchauhan1@gmail.com>
2023-01-15 22:24:00 +0530

    updating classwork contents

--- 2023/01_Jannuary/React/learningreact/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-// import App1 from './01_stateLifeCycle/App1';
-import App2 from './02_conditional/App2';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App2 />
-);
