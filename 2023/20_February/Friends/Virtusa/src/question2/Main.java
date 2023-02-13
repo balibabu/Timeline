f18618b3907db95162c5f150da8724986eed9afe
Bali Babu Chauhan <balibabuchauhan1@gmail.com>
2023-02-13 22:00:26 +0530

    updating classwork contents

--- 2023/20_February/Friends/Virtusa/src/question2/Main.java
+++ 2023/20_February/Friends/Virtusa/src/question2/Main.java
@@ -1,19 +1,30 @@
 package question2;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		int[][] mat1= {{1,2},{3,4}};
-		int[][] mat2= {{5,6},{7,8}};
-		int[][] result=new int[2][2];
-		Thread thread=new Thread(new RowMultiplyWorker(result, mat1, mat2, 2));
-		thread.run();
-		for(int[] row:result) {
-			for(int i:row) {
-				System.out.print(i+" ");
+		Scanner sc=new Scanner(System.in);
+		int m1=sc.nextInt();
+		int n1=sc.nextInt();
+		int m2=sc.nextInt();
+		int n2=sc.nextInt();
+		int[][] mat1= new int[m1][n1];
+		int[][] mat2= new int[m2][n2];
+
+		for(int i=0;i<m1;i++) {
+			for(int j=0;j<n1;j++) {
+				mat1[i][j]=sc.nextInt();
 			}
-			System.out.println();
 		}
+		for(int i=0;i<m2;i++) {
+			for(int j=0;j<n2;j++) {
+				mat2[i][j]=sc.nextInt();
+			}
+		}
+		Thread thread=new Thread(new MatrixMultiplication(mat1, mat2));
+		thread.run();
 	}
 
 }
--- 2023/20_February/Friends/Virtusa/src/question2/MatrixMultiplication.java
+++ 2023/20_February/Friends/Virtusa/src/question2/MatrixMultiplication.java
@@ -1,5 +1,28 @@
 package question2;
 
-public class MatrixMultiplication {
+public class MatrixMultiplication implements Runnable {
+	int[][] mat1;
+	int[][] mat2;
+
+	public MatrixMultiplication(int[][] mat1, int[][] mat2) {
+		super();
+		this.mat1 = mat1;
+		this.mat2 = mat2;
+	}
+
+
+	@Override
+	public void run() {
+		for (int i = 0; i < mat1.length; i++) {
+			for (int j = 0; j < mat1[0].length; j++) {
+				int temp = 0;
+				for (int k = 0; k < mat2.length; k++) {
+					temp += mat1[i][k] * mat2[k][j];
+				}
+				System.out.print(temp + " ");
+			}
+			System.out.println();
+		}
+	}
 
 }
