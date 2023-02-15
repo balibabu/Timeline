6d6d3f6480857e3ac5d08022a7a05e05f47aaa58
Bali Babu Chauhan <balibabuchauhan1@gmail.com>
2023-02-15 22:00:04 +0530

    updating classwork contents

--- 2023/02_February/Friends/Virtusa/src/question3/Main.java
+++ 2023/02_February/Friends/Virtusa/src/question3/Main.java
@@ -1,34 +1,28 @@
 package question3;
 
 public class Main {
-
 	public static void main(String[] args) {
 		Thread evenThread=new Thread(new EvenThread());
 		Thread oddThread=new Thread(new OddThread());
 		evenThread.run();
 		oddThread.run();
 	}
-
 }
 
 class EvenThread implements Runnable{
-
 	@Override
 	public void run() {
 		for(int i=2;i<=20;i+=2) {
 			System.out.println("Thread even: "+i);
 		}
 	}
-	
 }
 
 class OddThread implements Runnable{
-
 	@Override
 	public void run() {
 		for(int i=1;i<=20;i+=2) {
 			System.out.println("Thread Odd: "+i);
 		}
 	}
-	
 }
